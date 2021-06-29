0714bd3a6926ece4218f13e1389cffedecc57c3d
lyhper <lyhper@gmail.com>
2021-06-29 20:41:12 +0800

    feat: use cdn

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withLess = require('@zeit/next-less') // next-less目前只支持webpack 4
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = withLess({
-  cssModules: true
-})
+  cssModules: true, // css模块化
+  assetPrefix: isProd ? '' : '', // 静态资源前缀
+});
